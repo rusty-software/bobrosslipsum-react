a715946a6e3ef22ebd38433ffe6b0e4aff484fbe
Rusty Bentley <mycoding.alert@gmail.com>
2025-05-10 10:26:00 -0700

    removes old css files, updates current styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HomePage from "./HomePage";
-import "./final-styles.css";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
